5ec0509eb3bbe5f8beefe891e5f33a905664b680
kirushaRetar <118094156+KirushaRetarr@users.noreply.github.com>
2025-12-01 10:13:05 +0500

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <main className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold mb-4">
+          Бронирование билетов на развлекательные мероприятия
+        </h1>
+      </main>
+    </div>
+  );
+}
